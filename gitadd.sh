83fffdd820f0d0b884dfcc1b11af26ae380d6c16
limq-debian <gzlmq1@163.com>
2019-12-07 15:39:59 +0800

    2019-12-07 15:39:59  M  gitadd.sh A  gitadd2.sh A  source/_drafts/mytest.md A  source/_posts/2019-08-22-bbc-md.md A  source/_posts/2019-08-22-bbc-md/bbc.png A  source/_posts/2019-08-23-fu-base.md A  source/_posts/2019-08-23-wzdh.md A  source/_posts/2019-08-23-yuedu.assets/1566544858171.png A  source/_posts/2019-08-23-yuedu.md A  source/_posts/2019-08-23-yuedu/yuedu.jpg A  source/_posts/2019-11-08-Windows-cmd-bat.md A  source/_posts/2019-11-08-book.md A  source/_posts/2019-12-07-leaning.md A  source/_posts/2019-12-07-yuyanyishu.assets/image-20191207131838816.png A  source/_posts/2019-12-07-yuyanyishu.md A  themes/next/_config.20190822.yml

--- gitadd.sh
+++ gitadd.sh
@@ -1,7 +1,7 @@
 git status -s
 
-git status -s | awk ' $2 { print ( $2 ) }'>/c/Users/limq/addgit
-git add `more /c/Users/limq/addgit`
+git status -s | awk ' $2 { print ( $2 ) }'>/mnt/c/Users/limq/addgit
+git add `more /mnt/c/Users/limq/addgit`
 git status -s
 git commit -m " `date "+%Y-%m-%d %H:%M:%S"`  `git status -s`"
 git status -s
---
+++ source/_drafts/mytest.md
@@ -0,0 +1,4 @@
+---
+title: mytest
+tags:
+---
